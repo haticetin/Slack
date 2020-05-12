958a69ac777b6f0c962c652d48fd1e609a748756
haticetin <haticetin999@gmail.com>
2020-05-12 18:20:50 +0200

    add Windows.java

---
+++ src/Windows.java
@@ -0,0 +1,8 @@
+public class Windows {
+
+    public static void main(String[] args) {
+
+        System.out.println("I am using windows");
+        System.out.println("Windows vs MacOs");
+    }
+}
