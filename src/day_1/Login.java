af1762516a3a11793433ce4dac6f22ef1a02862a
haticetin <haticetin999@gmail.com>
2020-05-13 16:50:31 +0200

    first one

--- src/day_1/Login.java
+++ src/day_1/Login.java
@@ -5,17 +5,6 @@ public class Login {
     public static void main(String[] args) {
 
         // Slack users can login successfully
-
-        // Brand new changes frm GitHub
-
-        // this line is written from Intellij
-
-         
-            
-            
-            
-            
-      // this line is added from GitHub      
             
       
     }
