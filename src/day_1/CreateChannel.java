0f9ccad1a40cb9b5798a9787d2680684d8ecb232
haticetin <haticetin999@gmail.com>
2020-05-13 16:57:25 +0200

    update CreateChannel.java

--- src/day_1/CreateChannel.java
+++ src/day_1/CreateChannel.java
@@ -4,5 +4,14 @@ public class CreateChannel {
 
     public static void main(String[] args) {
 
+        // written from Intellij
+        int x = 0;
+        if(x==0){
+            System.out.println("equal");
+        }else{
+            System.out.println("not equual");
+        }
+
+
     }
 }
